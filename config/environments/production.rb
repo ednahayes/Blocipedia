ff6fc5bdfb2c2d6541833b2135881c1087927a0e
Edna <princessahe@hotmail.com>
2017-08-10 17:29:42 +0000

    new-checkpoint-2: User sign up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'fast-ocean-19157.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'radiant-savannah-47694.herokuapp.com' }
 end
